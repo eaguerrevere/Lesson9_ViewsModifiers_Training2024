9022c75fe7f039123dd8163b06710471c0cd5d0f
Eli <elizabeth.aguerrevere@gmail.com>
2024-04-21 14:25:41 -0700

    Initial Commit

---
+++ Lesson9_ViewsModifiers_Training2024/Lesson9_ViewsModifiers_Training2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson9_ViewsModifiers_Training2024App.swift
+//  Lesson9_ViewsModifiers_Training2024
+//
+//  Created by Elizabeth Aguerrevere on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson9_ViewsModifiers_Training2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
